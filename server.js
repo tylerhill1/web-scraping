abe6a623e18ad99603cefea7e89d8023ae4ed460
Tyler Hill <t.wakefield.hill@gmail.com>
2019-08-09 22:49:06 -0400

    Done

--- server.js
+++ server.js
@@ -81,6 +81,20 @@ app.get("/scrape", function(req, res) {
   });
 });
 
+// A GET route for scraping the echoJS website
+app.get("/", function(req, res) {
+  // Grab every document in the Articles collection
+  db.Article.find({})
+    .then(function(dbArticle) {
+      // If we were able to successfully find Articles, send them back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
 // Route for getting all Articles from the db
 app.get("/articles", function(req, res) {
   // Grab every document in the Articles collection
