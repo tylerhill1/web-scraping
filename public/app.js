51e690debc3d72122aac7bad5318fbabab5d4037
Tyler Hill <t.wakefield.hill@gmail.com>
2019-08-09 22:53:09 -0400

    fixing route paths

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 var scraped = false;
 
 // Grab the articles as a json
-$.getJSON("/articles", function(data) {
+$.getJSON("/", function(data) {
   // For each one
   $("#articles").empty();
   for (var i = 0; i < data.length; i++) {
